acc382200910e9820f00e960fff938d9050c42c1
Bhushan pawar <bapawar9409@gmail.com>
2026-09-06 20:24:31 +0530

    solve Anagram problem

--- java_practice/Anagram.java
+++ java_practice/Anagram.java
@@ -2,6 +2,29 @@ package java_practice;
 
 public class Anagram {
    public static void main(String[] args) {
-      
+
+      String st = "iiLL";
+      String st1 = "LiLi";
+
+      if (st.length() != st1.length()) {
+         System.out.println("Not Anagram");
+         return;
+      }
+
+      int[] frequency = new int[256];
+
+      for (int i = 0; i < st.length(); i++) {
+         frequency[st.charAt(i)]++;
+         frequency[st1.charAt(i)]--;
+      }
+
+      for (int count : frequency) {
+         if (count != 0) {
+            System.out.println("Not Anagram");
+            return;
+         }
+      }
+
+      System.out.println("Anagram");
    }
 }
---
+++ java_practice/Characterfrequency.java
@@ -0,0 +1,24 @@
+package java_practice;
+
+public class Characterfrequency {
+   public static void main(String[] args) {
+      String st="hellop";
+      int[] frequency=new int[26];
+      for(int i=0;i<st.length();i++){
+         frequency[st.charAt(i)-'a']++;
+      }
+      // for(int i=0;i<frequency.length;i++){
+
+      //    if(frequency[i]!=0){
+      //       System.out.println((char)(i+'a') + " " + frequency[i]);
+      //    }
+      // }
+
+      for(int i=0;i<st.length();i++){
+         if(frequency[st.charAt(i)-'a']!=0){
+             System.out.println(st.charAt(i)+" "+frequency[st.charAt(i) - 'a']);
+             frequency[st.charAt(i)-'a']=0;
+         }
+      }
+   }
+}
